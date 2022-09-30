b9e34ea73e71f8abe71ce521fecf2fa8dff6cbcb
syw1114 <syw1114>
2022-09-30 17:54:29 +0900

    header,Account (Study)

---
+++ src/main/java/com/stussy/stussyclone20220930syw/StussyClone20220930SywApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930syw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220930SywApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20220930SywApplication.class, args);
+	}
+
+}
